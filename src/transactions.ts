2e7ac65715374c4c75bd72c94eef5f1f9e9ab852
Haifaq8 <haifaa_q876@hotmail.com>
2025-12-10 19:35:30 +0300

    Update transactions.ts and package-lock.json

--- src/transactions.ts
+++ src/transactions.ts
@@ -19,55 +19,74 @@ const transactions: Transaction[] = [
 // - Returns a new array containing only the income transactions.
 // example:
 // filterIncomeTransactions(transactions); // => [["income", 1000], ["income", 1500], ["income", 700]]
-function filterIncomeTransactions(transactions: Transaction[]): Transaction[] {
-  // write your code here...
 
-  return []; // replace empty array with what you see is fit
-}
+  // write your code here...
+  
+  function filterIncomeTransactions(transactions: Transaction[]): Transaction[] {
+    return transactions.filter(([type]) => type === "income");
+  }
+  
 
 // `filterExpenseTransactions` function that:
 // - Accepts a "transactions" parameter of type "Transaction[]".
 // - Return a new array containing only the expense transactions.
 // example:
 // filterExpenseTransactions(transactions); // => [["expense", 500], ["expense", 300]]
-function filterExpenseTransactions(transactions: Transaction[]): Transaction[] {
-  // write your code here...
 
-  return []; // replace empty array with what you see is fit
-}
+  // write your code here...
+  function filterExpenseTransactions(transactions: Transaction[]): Transaction[] {
+    return transactions.filter(([type]) => type === "expense");
+  }
+  
 
 // `calculateTotalIncome` function that:
 // - Accepts a "transactions" parameter of type "Transaction[]".
 // - calculates the total income and returns it.
 // example:
 // calculateTotalIncome(transactions); // => 3200  (1000 + 1500 + 700)
-function calculateTotalIncome(transactions: Transaction[]): number {
-  // write your code here...
 
-  return -1; // replace -1 with what you see is fit
-}
+  // write your code here...
+  function calculateTotalIncome(transactions: Transaction[]): number {
+    return transactions.reduce((sum, [type, amount]) => {
+      if (type === "income") {
+        return sum + amount;
+      }
+      return sum;
+    }, 0);
+  }
+  
 
 // `calculateTotalExpenses` function that:
 // - Accepts a "transactions" parameter of type "Transaction[]".
 // - calculates the total expense and returns it.
 // example:
 // calculateTotalExpenses(transactions); // => 800  (500 + 300)
-function calculateTotalExpenses(transactions: Transaction[]): number {
   // write your code here...
-
-  return -1; // replace -1 with what you see is fit
-}
+  function calculateTotalExpenses(transactions: Transaction[]): number {
+    return transactions.reduce((sum, [type, amount]) => {
+      if (type === "expense") {
+        return sum + amount;
+      }
+      return sum;
+    }, 0);
+  }
+  
 
 // `calculateNetTotal` function that:
 // - Accepts a "transactions" parameter of type "Transaction[]".
 // - calculates the net total (total income - total expenses) and returns it.
 // example:
 // calculateNetTotal(transactions); // => 2400  (3200 - 800)
-function calculateNetTotal(transactions: Transaction[]): number {
-  // write your code here...
 
-  return -1; // replace -1 with what you see is fit
-}
+  // write your code here...
+  function calculateNetTotal(transactions: Transaction[]): number {
+    const income = calculateTotalIncome(transactions);
+    const expenses = calculateTotalExpenses(transactions);
+  
+    return income - expenses;
+  }
+  
+
 
 // `filterSignificantTransactions` function that:
 // - Accepts 2 parameters:
@@ -76,14 +95,16 @@ function calculateNetTotal(transactions: Transaction[]): number {
 // - identifies and creates a new array with transactions (both incomes and expenses) above or equal to "threshold".
 // example:
 // filterSignificantTransactions(transactions, 1000); // =>  [["income", 1000], ["income", 1500]]
-function filterSignificantTransactions(
-  transactions: Transaction[],
-  threshold: number
-): Transaction[] {
-  // write your code here...
 
-  return []; // replace empty array with what you see is fit
-}
+  // write your code here...
+  function filterSignificantTransactions(
+    transactions: Transaction[],
+    threshold: number
+  ): Transaction[] {
+    return transactions.filter(([_, amount]) => amount >= threshold);
+  }
+  
+
 
 export {
   Transaction,
